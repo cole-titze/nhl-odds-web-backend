70658a141b0d4d09d7a2d1ecf9ec2b34a1df41d8
Cole Titze <cole.titze@outlook.com>
2022-06-11 12:22:38 -0500

    Added models

---
+++ Entities/ViewModels/PredictedGameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Entities.ViewModels
+{
+	public class PredictedGameViewModel
+	{
+        public string homeTeamName { get; set; } = string.Empty;
+        public string awayTeamName { get; set; } = string.Empty;
+        public DateTime gameDate { get; set; }
+        public double vegasHomeOdds { get; set; }
+        public double vegasAwayOdds { get; set; }
+        public double modelHomeOdds { get; set; }
+        public double modelAwayOdds { get; set; }
+        // Eventually add Should I bet based on vegas and model odds
+    }
+}
+
