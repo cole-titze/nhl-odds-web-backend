7458ac221aa1be616770746e769ad6bf0a430ed3
Cole Titze <cole.titze@outlook.com>
2022-06-11 14:15:33 -0500

    Added action to api route

--- WebApi/Controllers/PredictedGameController.cs
+++ WebApi/Controllers/PredictedGameController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PredictedGameController
 	{
