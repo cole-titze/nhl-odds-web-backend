0d2c48c23a6b03e40ac726ff7206992b77483752
Cole Titze <cole.titze@outlook.com>
2022-06-11 13:35:55 -0500

    Added Database connection and Dependency Injection

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,14 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using DataAccess.PredictedGameRepository;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string _connectionString = builder.Configuration.GetConnectionString("PredictedGameDatabase");
 
 // Add services to the container.
+builder.Services.AddScoped<IPredictedGameRepository, PredictedGameRepository>();
+builder.Services.AddDbContext<PredictedGameDbContext>(x => x.UseSqlServer(_connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
